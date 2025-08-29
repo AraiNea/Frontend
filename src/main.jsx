0eb39a08805e57ce567830cae6054e5a9d0fca3a
lookchub <Ceekay662005@gmail.com>
2025-08-29 21:10:54 +0700

    Add js,bootstrap,Route,Css
    สร้างRouteขึ้นมาเพื่อไปหน้าต่างๆและนำเข้า javascript,css สำเร็จรูปที่อาจใช้ในอนาคต และนำเข้า bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Home.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Router>
+    <Routes>
+      {/* Public: No userInfo needed */}
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </Router>
 )
