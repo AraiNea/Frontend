3f113dc768532c835dec29c1a12d39de4da497d6
cxGDN <mink14122947@gmail.com>
2025-09-04 17:09:01 +0700

    create components&pages folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './Home.jsx'
+import Home from './pages/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
   <Router>
