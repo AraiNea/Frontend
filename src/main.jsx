0275215fb208c099a037fe9506334913fb20d579
มะงิ้ง <102567759+cxGDN@users.noreply.github.com>
2025-09-13 16:29:04 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home.jsx'
 
+
 createRoot(document.getElementById('root')).render(
   <Router>
     <Routes>
