e11f79e698ef7b11994ddca1d5289086a04cdef1
มะงิ้ง <102567759+cxGDN@users.noreply.github.com>
2025-09-27 20:18:59 +0700

    Update pizzaapp.cy.js

--- cypress/e2e/pizzaapp/pizzaapp.cy.js
+++ cypress/e2e/pizzaapp/pizzaapp.cy.js
@@ -2,11 +2,11 @@ describe('Navbar navigation', () => {
     it('clicks the 3rd nav-link', () => {
       
       cy.visit('http://localhost:3000/')
-  
+      
       
       cy.get(':nth-child(3) > .nav-link', { timeout: 10000 })
         .should('be.visible')  
-        .click()
+        .click({force: true})
       cy.get('.form-control').type('Water', { force: true })
       cy.get('.py-3').click({ force: true })
       cy.get(':nth-child(1) > .nav-link').click()
