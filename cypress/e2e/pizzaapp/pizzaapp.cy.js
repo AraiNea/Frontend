2eb37556bd693904f90f3f7fa556b58d358b1859
nampungggg <npueng04@gmail.com>
2025-09-27 12:52:54 +0700

    Update pizzaapp.cy.js

--- cypress/e2e/pizzaapp/pizzaapp.cy.js
+++ cypress/e2e/pizzaapp/pizzaapp.cy.js
@@ -1,45 +1,21 @@
 describe('Navbar navigation', () => {
-  it('clicks the 3rd nav-link and searches items', () => {
-    // เข้าเว็บ
-    cy.visit('http://localhost:5173/')
-    cy.get('body', { timeout: 10000 }).should('be.visible')
+    it('clicks the 3rd nav-link', () => {
+      
+      cy.visit('http://localhost:5173/')
+  
+      
+      cy.get(':nth-child(3) > .nav-link', { timeout: 10000 })
+        .should('be.visible')  
+        .click()
+      cy.get('.form-control').type('Water', { force: true })
+      cy.get('.py-3').click({ force: true })
+      cy.get(':nth-child(1) > .nav-link').click()
+      cy.get(':nth-child(3) > .nav-link', { timeout: 10000 })
+        .should('be.visible')  
+        .click()
+      cy.get('.form-control').type('BBQ', { force: true })
 
-    // คลิก nav-link ตัวที่ 3 ("Search") โดยใช้ contains แทน :nth-child
-    cy.contains('.nav-link', 'Search', { timeout: 10000 })
-      .should('be.visible')
-      .click()
 
-    // พิมพ์ใน input search
-    cy.get('.form-control', { timeout: 10000 })
-      .should('be.visible')
-      .and('be.enabled')
-      .type('Water')
 
-    // คลิกปุ่ม submit
-    cy.get('.py-3', { timeout: 10000 })
-      .should('be.visible')
-      .click({ force: true }) // ใช้ force เฉพาะกรณี element ถูก cover
-
-    // กลับไปหน้า Home
-    cy.contains('.nav-link', 'Home', { timeout: 10000 })
-      .should('be.visible')
-      .click()
-
-    // คลิก nav-link Search อีกครั้ง
-    cy.contains('.nav-link', 'Search', { timeout: 10000 })
-      .should('be.visible')
-      .click()
-
-    // พิมพ์ใน input search อีกครั้ง
-    cy.get('.form-control', { timeout: 10000 })
-      .should('be.visible')
-      .and('be.enabled')
-      .type('BBQ')
-
-    // คลิกปุ่ม submit อีกครั้ง
-    cy.get('.py-3', { timeout: 10000 })
-      .should('be.visible')
-      .click({ force: true })
+    })
   })
-})
-
