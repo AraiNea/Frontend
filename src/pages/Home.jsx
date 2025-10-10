b627b0230b7b1a4a265d85b8c5d7edf925686cfa
มะงิ้ง <102567759+cxGDN@users.noreply.github.com>
2025-10-11 01:07:31 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -61,8 +61,8 @@ function Home() {
                                 }}
                                 loop={heroData.length > 1}
                                 autoplay={{
-                                    delay: 3000,      // 3 วิ ต่อการเลื่อน
-                                    disableOnInteraction: false, // ให้เลื่อนต่อแม้ผู้ใช้ลากเอง
+                                    delay: 3000,
+                                    disableOnInteraction: false,
                                 }}
                                 className="hero-swiper"
                             >
@@ -77,8 +77,6 @@ function Home() {
                                                 >
                                                     {/* Badge */}
                                                     <span className="hero-badge">Recommended!!</span>
-
-<<<<<<< HEAD
                                                     {/* Image */}
                                                     <img
                                                         className="hero-image"
@@ -89,62 +87,19 @@ function Home() {
                                             </div>
                                         </SwiperSlide>
                                     ))}
-                                <div className="custom-swiper-button custom-swiper-button-prev">
-                                    <i className="bi bi-chevron-left"></i>
-                                </div>
-                                <div className="custom-swiper-button custom-swiper-button-next">
-                                    <i className="bi bi-chevron-right"></i>
-                                </div>
                             </Swiper>
-=======
-            {/* Hero Section */}
-            <section className="hero-pizza-section py-4">
-                <div className="container">
-                    <Swiper
-                        modules={[Navigation, Autoplay]}
-                        navigation={{
-                            prevEl: ".custom-swiper-button-prev",
-                            nextEl: ".custom-swiper-button-next",
-                        }}
-                        loop={heroData.length > 1}
-                        autoplay={{
-                            delay: 3000,      // 3 วิ ต่อการเลื่อน
-                            disableOnInteraction: false, // ให้เลื่อนต่อแม้ผู้ใช้ลากเอง
-                        }}
-                        className="hero-swiper"
-                    >
-                        {Array.isArray(heroData) &&
-                            heroData.map((p) => (
-                                <SwiperSlide key={p.recommendedId}>
-                                    <div className="d-flex justify-content-center">
-                                        <div 
-                                            className="hero-slide-content text-center"
-                                            style={{ cursor: "pointer" }}
-                                            onClick={() => navigate(`/product/${p.recommendedId}`)}
-                                        >
-                                            {/* Badge */}
-                                            <span className="hero-badge">Recommended!!</span>
-
-                                            {/* Image */}
-                                            <img
-                                                className="hero-image"
-                                                src={`http://localhost:8080${p.recommendImgPath}`}
-                                                alt={`Recommended ${p.recommendedId}`}
-                                            />
-                                        </div>
-                                    </div>
-                                </SwiperSlide>
-                            ))}
-                        <div className="custom-swiper-button custom-swiper-button-prev">
-                            <i className="bi bi-chevron-left"></i>
->>>>>>> 3b46dc3d46751d09a8dbef437e8a312e2e0839cb
+                            <div className="custom-swiper-button custom-swiper-button-prev">
+                                <i className="bi bi-chevron-left"></i>
+                            </div>
+                            <div className="custom-swiper-button custom-swiper-button-next">
+                                <i className="bi bi-chevron-right"></i>
+                            </div>
                         </div>
                     </section>
-
                     {/* Body */}
                     <CatalogGrid data={data} onProductClick={(id) => navigate(`/product/${id}`)} />
-                    <Footer />
                 </main>
+                <Footer />
             </div>
         </>
     );
