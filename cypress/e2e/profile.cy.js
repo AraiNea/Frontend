9be7a3e698e4122e7a1fe08e1b4c35d3812da22d
nampungggg <npueng04@gmail.com>
2025-10-16 22:20:39 +0700

    Update profile.cy.js

--- cypress/e2e/profile.cy.js
+++ cypress/e2e/profile.cy.js
@@ -2,12 +2,15 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000/profile')
   })
+
   it('assign', () => {
     cy.visit('http://localhost:3000/profile')
-    cy.get('[name="profileName"]').type('Daraporn')
-    cy.get('[name="username"]').type('aikeaw')
-    cy.get('[name="profileSname"]').type('Saepoo')
-    cy.get('[name="password"]').type('12345')
-    
+
+    // ✅ รอให้ SweetAlert หายก่อนเริ่มกรอก
+    cy.get('.swal2-confirm').click()
+    cy.get('[name="profileName"]').should('be.visible').type('Daraporn')
+    cy.get('[name="username"]').should('be.visible').type('aikeaw')
+    cy.get('[name="profileSname"]').should('be.visible').type('Saepoo')
+    cy.get('[name="password"]').should('be.visible').type('12345')
   })
 })
