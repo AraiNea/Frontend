c6bd966c003656be88e9e8d0330a41d0db91ae9f
nampungggg <npueng04@gmail.com>
2025-10-16 23:08:10 +0700

    Create login.cy.js

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,19 @@
+
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000')
+  })
+  it('go to login', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('.header-action-btn > .d-none').click({force:true})
+    cy.get('.dropdown-item').click({force:true})
+   
+  })
+  it('Assign', () => {
+    cy.visit('http://localhost:3000/login')
+    cy.get(':nth-child(2) > .form-control').type('Jirayu')
+    cy.get(':nth-child(3) > .form-control').type('Jaidee')
+    cy.get('.btn-danger').click()
+    cy.get('.swal2-confirm').click()
+  })
+})
