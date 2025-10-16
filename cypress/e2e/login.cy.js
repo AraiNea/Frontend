b34360312ec80c775ec671da748db61045a2fdc3
nampungggg <npueng04@gmail.com>
2025-10-17 00:03:20 +0700

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,12 +3,12 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000')
   })
-  it('go to login', () => {
-    cy.visit('http://localhost:3000')
-    cy.get('.header-action-btn > .d-none').click({force:true})
-    cy.get('.dropdown-item').click({force:true})
+  // it('go to login', () => {
+  //   cy.visit('http://localhost:3000')
+  //   cy.get('.header-action-btn > .d-none').click({force:true})
+  //   cy.get('.dropdown-item').click({force:true})
    
-  })
+  // })
   it('Assign', () => {
     cy.visit('http://localhost:3000/login')
 
