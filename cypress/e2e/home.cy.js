f7312530ff27a713f742e22199a93b2f78cde2d1
nampungggg <npueng04@gmail.com>
2025-10-16 21:48:26 +0700

    update e2e home

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -4,18 +4,26 @@ describe('template spec', () => {
   })
   it('click herosection',()=>{
     cy.visit('http://localhost:3000')
-    cy.get('.swiper-slide-active > .d-flex > .hero-slide-content > .hero-image').click()
-    cy.get('.container > .d-flex').click()
+    cy.get('.swiper-slide-active > .d-flex > .hero-slide-content > .hero-image').click({ force: true })
+    cy.get('.container > .d-flex').click({ force: true })
   })
   it('slide herosection',()=>{
     cy.visit('http://localhost:3000')
-    cy.get('.custom-swiper-button-prev').click()
+    cy.get('.custom-swiper-button-prev').click({ force: true })
     
   })
   it('click navbar',()=>{
     cy.visit('http://localhost:3000')
-    cy.get(':nth-child(2) > .nav-link').click()
+    cy.get(':nth-child(2) > .nav-link').click({ force: true })
     cy.visit('http://localhost:3000/category')
+    
+  })
+
+  it('click pizza',()=>{
+    cy.visit('http://localhost:3000')
+    cy.get(':nth-child(1) > .row > :nth-child(1) > .card > .card-body').click({ force: true })
+    cy.visit('http://localhost:3000/product/1')
+    cy.get('.container > .d-flex').click({ force: true })
     
   })
 })
