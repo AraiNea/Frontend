b6e3ca71fc5972cd0ba9628b973525f7f17c007e
nampungggg <npueng04@gmail.com>
2025-10-16 21:18:00 +0700

    e2e cate page

--- cypress/e2e/pizzaapp/pizzaapp.cy.js
+++ cypress/e2e/pizzaapp/pizzaapp.cy.js
@@ -21,8 +21,53 @@ describe('Navbar and Category E2E', () => {
   it('god pizza', () => {
     cy.visit('http://localhost:3000/')
 
-    cy.get(':nth-child(1) > .row > :nth-child(1) > .card > .card-body').click()
+    cy.get(':nth-child(1) > .row > :nth-child(1) > .card > .card-body').click({ force: true })
   })
   
+  it('god back', () => {
+    cy.visit('http://localhost:3000/product/1')
+    cy.get('.container > .d-flex').click()
+
+    
+  })
+  it('god navbar2', () => {
+    cy.visit('http://localhost:3000/category')
+    cy.get('.category-2').click({ force: true })
+
+    
+  })
+  it('god garlicbread', () => {
+    cy.visit('http://localhost:3000/category/appetizer')
+    cy.get(':nth-child(1) > .card > .card-body').click()
+
+    
+  })
+  it('god back', () => {
+    cy.visit('http://localhost:3000/product/6')
+    cy.get('.container > .d-flex').click()
+
+    
+  })
+
+  it('god navbar3', () => {
+    cy.visit('http://localhost:3000/category')
+    cy.get('.category-3').click({ force: true })
+
+    
+  })
+
+  it('god garlicbread', () => {
+    cy.visit('http://localhost:3000/category/drink')
+    cy.get(':nth-child(1) > .card > .card-body').click()
+
+    
+  })
+
+  it('god back', () => {
+    cy.visit('http://localhost:3000/product/9')
+    cy.get('.container > .d-flex').click()
+
+    
+  })
 
 })
