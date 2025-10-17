c2023ab91ac9780054afae21dbd164febedd5df1
มะงิ้ง <102567759+cxGDN@users.noreply.github.com>
2025-10-17 15:24:51 +0700

    Update profile.cy.js

--- cypress/e2e/profile.cy.js
+++ cypress/e2e/profile.cy.js
@@ -7,13 +7,12 @@ describe('template spec', () => {
     cy.visit('http://localhost:3000/profile')
 
     // ✅ รอให้ SweetAlert หายก่อนเริ่มกรอก
-    cy.get('.swal2-confirm').click()
-    cy.get('[name="profileName"]').should('be.visible').type('Daraporn')
-    cy.get('[name="username"]').should('be.visible').type('aikeaw')
-    cy.get('[name="profileSname"]').should('be.visible').type('Saepoo')
-    cy.get('[name="password"]').should('be.visible').type('12345')
-    cy.get('.mt-4 > .btn').click()
-    cy.get('.swal2-confirm').click()
-
+    cy.get('.swal2-confirm').click({ force: true })
+    cy.get('[name="profileName"]').should('be.visible').type('Daraporn', { force: true })
+    cy.get('[name="username"]').should('be.visible').type('aikeaw', { force: true })
+    cy.get('[name="profileSname"]').should('be.visible').type('Saepoo', { force: true })
+    cy.get('[name="password"]').should('be.visible').type('12345', { force: true })
+    cy.get('.mt-4 > .btn').click({ force: true })
+    cy.get('.swal2-confirm').click({ force: true })
   })
 })
