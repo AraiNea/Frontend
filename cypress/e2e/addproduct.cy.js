6623c69adc1ecbefa260793bcf0baea4f430347e
nampungggg <npueng04@gmail.com>
2025-11-21 23:56:17 +0700

    Update addproduct.cy.js

--- cypress/e2e/addproduct.cy.js
+++ cypress/e2e/addproduct.cy.js
@@ -1,61 +1,43 @@
 describe('template spec', () => {
+  const username = 'alice';
+  const password = 'alice123';
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+    // Login step
+    cy.get('#header span.d-none').click({ force: true });
+    cy.get('#header button.dropdown-item').click({ force: true });
+    cy.get('#root input[placeholder="Value"][type="text"]').click({ force: true });
+    cy.contains('label', 'Username').next('input.form-control').type(username, { force: true });
+    cy.get('#root input[type="password"]').click({ force: true });
+    cy.get('#root input[type="password"]').type(password, { force: true });
+    cy.get('#root button.btn-danger').click({ force: true });
+  });
+
   it('passes', () => {
-    cy.visit('http://localhost:3000')
-  })
+    // แค่เข้าเว็บ
+    cy.visit('http://localhost:3000');
+  });
 
   it('cart', () => {
-    cy.visit('http://localhost:3000')
-    cy.get('#header span.d-none').click({ force: true });
-    cy.get('#header button.dropdown-item').click({ force: true });
-    cy.get('#root input[placeholder="Value"][type="text"]').click({ force: true });
-    cy.contains('label', 'Username').next('input.form-control').type('alice', { force: true });
-    cy.get('#root input[type="password"]').click({ force: true });
-    cy.get('#root input[type="password"]').type('alice123', { force: true });
-    cy.get('#root button.btn-danger').click({ force: true });
-    cy.get('#root img[alt="BaconHam Cheese"]').click({ force: true });
-    cy.get('#root button.w-100').click({ force: true });
-    cy.get('button.form-Button-Swal').click({ force: true });
-  })
+    cy.get('#root img[alt="BaconHam Cheese"]').should('be.visible').click();
+    cy.get('#root button.w-100').should('be.visible').click();
+    cy.get('button.form-Button-Swal').should('be.visible').click();
+  });
 
   it('dppicate cart', () => {
-    cy.visit('http://localhost:3000')
-    cy.get('#header span.d-none').click({ force: true });
-    cy.get('#header button.dropdown-item').click({ force: true });
-    cy.get('#root input[placeholder="Value"][type="text"]').click({ force: true });
-    cy.contains('label', 'Username').next('input.form-control').type('alice', { force: true });
-    cy.get('#root input[type="password"]').click({ force: true });
-    cy.get('#root input[type="password"]').type('alice123', { force: true });
-    cy.get('#root button.btn-danger').click({ force: true });
-    cy.get('#root img[alt="BaconHam Cheese"]').click({ force: true });
-    cy.get('#root button.w-100').click({ force: true });
-    cy.get('button.form-Button-Swal').click({ force: true });
-    
-  })
+    cy.get('#root img[alt="BaconHam Cheese"]').should('be.visible').click();
+    cy.get('#root button.w-100').should('be.visible').click();
+    cy.get('button.form-Button-Swal').should('be.visible').click();
+  });
 
   it('modify cart', () => {
-    cy.visit('http://localhost:3000')
-    cy.get('#header span.d-none').click({ force: true });
-    cy.get('#header button.dropdown-item').click({ force: true });
-    cy.get('#root input[placeholder="Value"][type="text"]').click({ force: true });
-    cy.contains('label', 'Username').next('input.form-control').type('alice', { force: true });
-    cy.get('#root input[type="password"]').click({ force: true });
-    cy.get('#root input[type="password"]').type('alice123', { force: true });
-    cy.get('#root button.btn-danger').click({ force: true });
-    cy.get('#header button.position-relative span.d-none').click();
-    cy.get('#root i.bi-plus-circle').click({ force: true });
-    cy.get('#root button.cart-checkout-btn').click({ force: true });
-        
-  })
+    cy.get('#header button.position-relative span.d-none').should('be.visible').click();
+    cy.get('#root i.bi-plus-circle').should('be.visible').click();
+    cy.get('#root button.cart-checkout-btn').should('be.visible').click();
+  });
 
   it('water soldout', () => {
-    cy.visit('http://localhost:3000')
-    cy.get('#header span.d-none').click({ force: true });
-    cy.get('#header button.dropdown-item').click({ force: true });
-    cy.get('#root input[placeholder="Value"][type="text"]').click({ force: true });
-    cy.contains('label', 'Username').next('input.form-control').type('alice', { force: true });
-    cy.get('#root input[type="password"]').click({ force: true });
-    cy.get('#root input[type="password"]').type('alice123', { force: true });
-    cy.get('#root button.btn-danger').click({ force: true });
-    cy.get('#root img[alt="Water"]').click();
-  })
-})
+    cy.get('#root img[alt="Water"]').should('be.visible').click();
+  });
+});
